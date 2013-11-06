da01d6e681a0d8b322cf62de49d9265840c9f717
Michael Bianco <iloveitaly@gmail.com>
2013-11-05 21:13:12 -0500

    Bootstrap 3 form template formatting

--- templates/widget_template.php
+++ templates/widget_template.php
@@ -1,15 +1,34 @@
-<form action="<?php echo $_SERVER['REQUEST_URI']; ?>" id="<?php echo $this->id_base . '_form-' . $this->number; ?>" method="post">
-	<?php echo $this->subscribe_errors; ?>
-	<?php	if ($instance['collect_first']): ?>
-	<input placeholder="First Name" class="input-small" type="text" name="<?php echo $this->id_base . '_first_name'; ?>" />
-	<?php endif; ?>
-	<?php if ($instance['collect_last']): ?>
-	<input placeholder="Last Name" class="input-small" type="text" name="<?php echo $this->id_base . '_last_name'; ?>" /></label>
-	<?php	endif; ?>
-	<br/>
+<?php
+$form_id = $this->id_base . '_form-' . $this->number;
+?>
+<form action="<?php echo $_SERVER['REQUEST_URI']; ?>" id="<?php echo $form_id; ?>" method="post">
 	<input type="hidden" name="ns_mc_number" value="<?php echo $this->number; ?>" />
-	<input placeholder="Email Address" id="<?php echo $this->id_base; ?>-email-<?php echo $this->number; ?>" type="text" name="<?php echo $this->id_base; ?>_email" /><br/>
+
+	<?php echo $this->subscribe_errors; ?>
+
+	<?php	if($instance['collect_first']): ?>
+	<div class="form-group">
+		<input class='form-control' placeholder="First Name" type="text" name="<?php echo $this->id_base . '_first_name'; ?>" />
+	</div>
+	<?php endif; ?>
+
+	<?php if ($instance['collect_last']): ?>
+	<div class="form-group">
+		<input class='form-control' placeholder="Last Name" type="text" name="<?php echo $this->id_base . '_last_name'; ?>" />
+	</div>
+	<?php	endif; ?>
+
+	<div class="form-group">
+		<label>Email Address</label>
+		<input class='form-control' placeholder="Enter Email Address" id="<?php echo $this->id_base; ?>-email-<?php echo $this->number; ?>" type="text" name="<?php echo $this->id_base; ?>_email" />
+	</div>
+
 	<input class="btn btn-success" type="submit" name="<?php echo __($instance['signup_text'], 'mailchimp-widget'); ?>" value="<?php echo __($instance['signup_text'], 'mailchimp-widget'); ?>" />
 </form>
-<script>jQuery('#<?php echo $this->id_base; ?>_form-<?php echo $this->number; ?>').ns_mc_widget({"url" : "<?php echo $_SERVER['PHP_SELF']; ?>", "cookie_id" : "<?php echo $this->id_base; ?>-<?php echo $this->number; ?>", "cookie_value" : "<?php echo $this->hash_mailing_list_id(); ?>", "loader_graphic" : "<?php echo $this->default_loader_graphic; ?>"});
+<script>jQuery('#<?php echo $form_id; ?>').ns_mc_widget({
+	"url":"<?php echo $_SERVER['PHP_SELF']; ?>",
+	"cookie_id" : "<?php echo $this->id_base; ?>-<?php echo $this->number; ?>",
+	"cookie_value" : "<?php echo $this->hash_mailing_list_id(); ?>",
+	"loader_graphic" : "<?php echo $this->default_loader_graphic; ?>"
+});
 </script>
